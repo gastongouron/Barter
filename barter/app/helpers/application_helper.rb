71808c4609aa54d94099692ca7b4842a694a533d
gastongouron <gastongouron@gmail.com>
2016-02-14 17:47:32 +0100

    Add title functionality for project show page

--- barter/app/helpers/application_helper.rb
+++ barter/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Barter").join(" - ")
+      end
+    end
+  end
+
 end
--- barter/spec/features/creating_swaps_spec.rb
+++ barter/spec/features/creating_swaps_spec.rb
@@ -7,5 +7,10 @@ RSpec.feature "Users can create new swaps" do
     fill_in "Description", with: "A text editor for everyone"
     click_button "Create Swap"
     expect(page).to have_content "Sublime Text 3 A text editor for everyone Project has been created."
+    swap = Swap.find_by(name: "Sublime Text 3")
+    expect(page.current_url).to eq swap_url(swap)
+
+    title = "Sublime Text 3 - Swaps - Barter"
+    expect(page).to have_title title
   end
 end
