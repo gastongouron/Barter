cc137533170a907ec4c0886bcf6c8d1a5617cc99
gastongouron <gastongouron@gmail.com>
2016-02-14 23:39:06 +0100

    Implement bart display

---
+++ barter/spec/factories/bart_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :bart do
+    name "Example bart"
+    description "An example bart, nothing more"
+  end
+end
---
+++ barter/spec/features/viewing_rickets_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Users can view barts" do
+
+  before do
+
+    sublime = FactoryGirl.create(:swap, name: "Sublime Text 3")
+    FactoryGirl.create(:bart, swap: sublime,
+      name: "Make it shiny!",
+      description: "Gradients! Starbursts! Oh my!")
+    ie = FactoryGirl.create(:swap, name: "Internet Explorer")
+    FactoryGirl.create(:bart, swap: ie,
+      name: "Standards compliance", description: "Isn't a joke.")
+    visit "/" end
+
+    scenario "for a given swap" do
+      click_link "Sublime Text 3"
+      expect(page).to have_content "Make it shiny!"
+      expect(page).to_not have_content "Standards compliance"
+      click_link "Make it shiny!"
+
+      within("#bart h2") do
+        expect(page).to have_content "Make it shiny!"
+      end
+      expect(page).to have_content "Gradients! Starbursts! Oh my!"
+    end
+  end
