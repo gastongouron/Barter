bc016c0bf23184478413384e57a3f3a6d87fa24d
gastongouron <gastongouron@gmail.com>
2016-02-15 08:24:21 +0100

    Implemented sign up.

--- barter/config/routes.rb
+++ barter/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   root "swaps#index"
 
   resources :swaps do
     resources :barts
   end
+
+  resources :users
 
 end
