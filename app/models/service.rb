33651c84fe1fd6c9dfd25bcf575df53e11b3bd96
gastongouron <gastongouron@gmail.com>
2016-02-29 21:09:37 +0100

    change service model self.update

--- app/models/service.rb
+++ app/models/service.rb
@@ -23,21 +23,25 @@ class Service < ActiveRecord::Base
   end
 
   def self.credit_service
-    p ended_jobs = Service.where(archived: true, credited: false)
-
+    ended_jobs = Service.where(archived: true, credited: false)
     users_to_credit_once = []
 
     ended_jobs.each do |job|
-     users_to_credit_once << job.helper_id
-     job.credited = true
-     job.save
+      if job.helper_id != nil
+        users_to_credit_once << job.helper_id
+      else
+        users_to_credit_once << job.servicer_id
+      end
+    job.credited = true
+    job.save
+  end
+
+  users_to_credit_once.each do |user_id|
+    user = User.find_by(id: user_id)
+    user.timecoin += 1
+    user.save
     end
 
-    users_to_credit_once.each do |user_id|
-      user = User.find_by(id: user_id)
-      user.timecoin += 1
-      user.save
-    end
   end
 
 end
